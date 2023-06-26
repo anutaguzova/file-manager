474a58a39fceffff377605a136a379f066a81a8e
Hanna Huzava <guzovaann@gmail.com>
2023-06-27 01:19:10 +0200

    feat :add fucntion copyfile()

--- src/fileOperations.js
+++ src/fileOperations.js
@@ -1,7 +1,7 @@
 import { writeFile} from "node:fs/promises";
 import { rename } from 'node:fs/promises';
 import { unlink } from 'node:fs/promises';
-import { createReadStream} from "node:fs";
+import { createReadStream, createWriteStream} from "node:fs";
 import { displayInvalidInputMessage, printWorkingDirectory } from "./messageHelper.js";
 
 
@@ -28,6 +28,21 @@ export const addFile = async (filename) => {
         await writeFile(filename, fileContent, {flag: 'wx'});
         console.log(`${filename} was successfully created`)
     } catch (error) {
+        displayInvalidInputMessage();
+    }
+};
+
+export const copyFile = async (filePath, newFilePath) => {
+    try {
+        const inp = createReadStream(filePath);
+        const out = createWriteStream(newFilePath);
+        inp.pipe(out);
+
+        await new Promise((resolve) => {out.on('finish', resolve)});
+        console.log('File copied');
+        printWorkingDirectory();
+    } catch (error) {
+        console.log(error);
         displayInvalidInputMessage();
     }
 };
